1483862a562477c18387c653596b003d4879736e
Pierre Marchand <pierre.m@atelier-cartographique.be>
2025-01-23 09:16:48 +0100

    fixed engine not cleared /2

--- server/client/src/engine.ts
+++ server/client/src/engine.ts
@@ -1,15 +1,13 @@
-import { emptyElement, events } from "./lib/dom";
+import { events } from "./lib/dom";
 import { DIV, replaceNodeContent, SPAN } from "./lib/html";
 import { formatMove } from "./san";
 import { assign, get, subscribe } from "./store";
 
-const render = (root: HTMLElement) => {
-  emptyElement(root);
+const render = (engineInfo: HTMLElement, engineState: HTMLElement) => {
   const state = get("engine");
-  const engineInfo = DIV("info", SPAN("name", get("engineName")));
-  const engineState = DIV("state");
   const setEngine = replaceNodeContent(engineState);
-  root.append(engineInfo, engineState);
+  const setEngineInfo = replaceNodeContent(engineInfo);
+  setEngineInfo(SPAN("name", get("engineName")));
   switch (state._tag) {
     case "idle":
       return setEngine("·");
@@ -21,11 +19,13 @@ const render = (root: HTMLElement) => {
 };
 
 export const mountEngine = (root: HTMLElement) => {
-  const engine = events(DIV("engine"), (add) =>
+  const engineInfo = DIV("info");
+  const engineState = DIV("state");
+  const engine = events(DIV("engine", engineInfo, engineState), (add) =>
     add("click", () => assign("screen", "movelist"))
   );
-  render(engine);
-  subscribe("engine")(() => render(engine));
 
+  subscribe("engine", "engineName")(() => render(engineInfo, engineState));
+  render(engineInfo, engineState);
   root.append(engine);
 };
