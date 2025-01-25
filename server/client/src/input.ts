5ccb5060e8b4c4d9c31911a7300d5b034446ba90
Pierre Marchand <pierre.m@atelier-cartographique.be>
2025-01-25 21:00:59 +0100

    chipos style

--- server/client/src/input.ts
+++ server/client/src/input.ts
@@ -177,11 +177,11 @@ const renderMoves2 = (selected: Nullable<Role>, moveList: Move[]) => {
             DIV(`square ${square} target`, DIV("label", square.toLowerCase())),
             (add) =>
               add("click", () => {
-                if (tos.length > 1) {
-                  renderSelect(tos);
-                } else {
-                  playMove(tos[0]);
-                }
+                // if (tos.length > 1) {
+                renderSelect(tos);
+                // } else {
+                // playMove(tos[0]);
+                // }
               })
           );
         }
