35061a15695a4c0a0d614da99125bb738a0fa701
Pierre Marchand <pierre.m@atelier-cartographique.be>
2025-01-25 23:05:35 +0100

    intro text

--- server/client/src/home.ts
+++ server/client/src/home.ts
@@ -1,6 +1,6 @@
 import { startGame } from "./game";
 import { events } from "./lib/dom";
-import { ANCHOR, DIV } from "./lib/html";
+import { ANCHOR, DIV, SPAN } from "./lib/html";
 import { connect } from "./play";
 import { assign } from "./store";
 
@@ -23,6 +23,17 @@ const footer = DIV(
   ANCHOR("link", "https://github.com/pierremarc/ucui", "Source code & feedback")
 );
 
+const intro = DIV(
+  "intro",
+  SPAN("ucui", "µcui "),
+  `
+  is there to train or play with a chess engine over the board, 
+  when the purse can't afford a fine elctronic chess set. 
+  It aims to be as little disruptive as possible, please 
+  try and give us feedback. 
+    `
+);
+
 export const mountHome = (root: HTMLElement) => {
-  root.append(DIV("home", buttonPlay, button_config, footer));
+  root.append(DIV("home", intro, buttonPlay, button_config, footer));
 };
