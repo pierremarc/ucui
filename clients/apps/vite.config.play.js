48f95c7af68d2abab660af1bef7fe2e432ede68e
Pierre Marchand <pierre.m@atelier-cartographique.be>
2025-02-01 11:11:04 +0100

    vite config

--- clients/apps/vite.config.play.js
+++ clients/apps/vite.config.play.js
@@ -5,17 +5,20 @@ import checker from "vite-plugin-checker";
 
 const name = "play";
 
-export default defineConfig({
+export default defineConfig(({ command }) => ({
   root: resolve(__dirname, name),
   publicDir: resolve(__dirname, "assets"),
   base: `/${name}/`,
   build: {
     outDir: resolve(__dirname, "dist", name),
   },
-  // plugins: [basicSsl()],
-  plugins: [
-    checker({
-      typescript: true,
-    }),
-  ],
-});
+  plugins:
+    command === "serve"
+      ? [
+          // basicSsl(),
+          checker({
+            typescript: true,
+          }),
+        ]
+      : [],
+}));
