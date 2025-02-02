45f633d51272c1aa986d1768c48ea8abeaa86415
Pierre Marchand <pierre.m@atelier-cartographique.be>
2025-02-02 12:12:30 +0100

    a less intrusive score indicator

--- clients/apps/play/engine.ts
+++ clients/apps/play/engine.ts
@@ -1,18 +1,36 @@
-import { events } from "../lib/dom";
+import { attrs, events } from "../lib/dom";
 import { replaceNodeContent, SPAN, DIV } from "../lib/html";
 import { EngineScore } from "../lib/ucui/types";
 import { formatMove } from "./san";
 import { assign, get, getPlayerColor, getTurn, subscribe } from "./store";
 
+// See https://www.chessprogramming.org/Pawn_Advantage,_Win_Percentage,_and_Elo
+const normalizeCentipawns = (cp: number) => {
+  const p = cp / 100;
+  const d = 1 + Math.pow(10, -p / 4);
+  return 1 / d;
+};
+
+const centipawns = (cp: number) => {
+  // score is always given from engine's point of view
+  // https://backscattering.de/chess/uci/#engine-info-score
+  const n = normalizeCentipawns(cp);
+  const d = Math.round(255 * n);
+  return attrs(DIV("score-cp"), (set) => {
+    set("style", `background-color: rgb(${d},${d},${d});`);
+    set("title", (cp / 100).toFixed(2));
+  });
+};
+
 const renderScore = (score: EngineScore) => {
   switch (score._tag) {
     case "None":
-      return DIV(" score-none", "??");
+      return DIV("score-none", "??");
     case "CentiPawns":
-      return DIV(" score-cp", (score.score / 100).toFixed(1));
+      return centipawns(score.score);
     case "Mate":
       return DIV(
-        " score-mate ",
+        "score-mate ",
         score.moves < 0
           ? `Engine fears a mate in ${Math.abs(score.moves)}`
           : `Engine sees you  mate in ${score.moves}`
